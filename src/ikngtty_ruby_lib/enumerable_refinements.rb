0172c0e3c9c8e4bce6fb8a135507c294d636ada7
ikngtty <ikngtty@gmail.com>
2018-05-01 20:55:50 +0900

    Add 'take_break_if'.

--- src/ikngtty_ruby_lib/enumerable_refinements.rb
+++ src/ikngtty_ruby_lib/enumerable_refinements.rb
@@ -4,6 +4,32 @@ module IkngttyRubyLib
       def empty?
         self.first(1).length == 0
       end
+
+      # Similar to reversed "take_while", so to say "take_unless",
+      # but takes one more element, which evaluates to true first.
+      def take_break_if(&block)
+        Enumerator.new do |y|
+          took = []
+          self.each do |element|
+            took << element
+            break if y.yield(element)
+          end
+          took
+        end.each(&block)
+      end
+    end
+
+    refine Enumerator::Lazy do
+      def take_break_if(&block)
+        Enumerator.new do |y_for_break|
+          Enumerator.new do |y_for_take|
+            self.each do |element|
+              y_for_take << element
+              break if y_for_break.yield(element)
+            end
+          end.lazy
+        end.lazy.each(&block)
+      end
     end
 
     refine Array do
--- test/ikngtty_ruby_lib/enumerable_refinements_test.rb
+++ test/ikngtty_ruby_lib/enumerable_refinements_test.rb
@@ -38,6 +38,122 @@ module IkngttyRubyLibTest
       end.call
     end
 
+    def test_enumerable_take_break_if_with_block
+      # Takes elements while took one evaluates to false, and at last
+      # takes one which evaluates to true first.
+      lambda do
+        took = [10, 20, 30, 40, 50].take_break_if { |e| e >= 30 }
+        assert_equal([10, 20, 30], took)
+      end.call
+
+      # Takes at least one element, even if all evaluate to true.
+      lambda do
+        took = [10, 20, 30, 40, 50].take_break_if { |e| e >= 0 }
+        assert_equal([10], took)
+      end.call
+
+      # When nothing evaluates to true, takes all.
+      lambda do
+        took = [10, 20, 30, 40, 50].take_break_if { |e| e >= 100 }
+        assert_equal([10, 20, 30, 40, 50], took)
+      end.call
+
+      # When empty, returns empty.
+      lambda do
+        took = [].take_break_if { |e| e >= 0 }
+        assert_equal([], took)
+      end.call
+    end
+
+    def test_enumerable_take_break_if_without_block
+      will_take = [10, 20, 30, 40, 50].take_break_if
+      assert_instance_of(Enumerator, will_take)
+
+      took = will_take.with_index { |e, i| i >= 2 }
+      assert_equal([10, 20, 30], took)
+    end
+
+    def test_enumerator_lazy_take_break_if_with_block
+      # Takes elements while took one evaluates to false, and at last
+      # takes one which evaluates to true first.
+      lambda do
+        took = [10, 20, 30, 40, 50].lazy.take_break_if { |e| e >= 30 }.force
+        assert_equal([10, 20, 30], took)
+      end.call
+
+      # Takes at least one element, even if all evaluate to true.
+      lambda do
+        took = [10, 20, 30, 40, 50].lazy.take_break_if { |e| e >= 0 }.force
+        assert_equal([10], took)
+      end.call
+
+      # When nothing evaluates to true, takes all.
+      lambda do
+        took = [10, 20, 30, 40, 50].lazy.take_break_if { |e| e >= 100 }.force
+        assert_equal([10, 20, 30, 40, 50], took)
+      end.call
+
+      # When empty, returns empty.
+      lambda do
+        took = [].lazy.take_break_if { |e| e >= 0 }.force
+        assert_equal([], took)
+      end.call
+
+      # Evaluates lazy.
+      lambda do
+        sub_effects = []
+
+        will_take = [10, 20, 30, 40, 50].lazy.take_break_if do |e|
+          sub_effects << 'first'
+          e >= 30
+        end
+        assert_instance_of(Enumerator::Lazy, will_take)
+        assert_equal([], sub_effects)
+
+        took = will_take.select do |e|
+          sub_effects << 'second'
+          e % 20 == 10
+        end.force
+        assert_equal([10, 30], took)
+        assert_equal(%w[
+          second
+          first
+          second
+          first
+          second
+          first
+        ], sub_effects)
+      end
+    end
+
+    def test_enumerator_lazy_take_break_if_without_block
+      sub_effects = []
+
+      will_will_take = [10, 20, 30, 40, 50].lazy.take_break_if
+      assert_instance_of(Enumerator::Lazy, will_will_take)
+
+      will_take = will_will_take.with_index do |e, i|
+        sub_effects << 'first'
+        i >= 2
+      end
+      assert_instance_of(Enumerator::Lazy, will_take)
+      assert_equal([], sub_effects)
+
+      took = will_take.select do |e|
+        sub_effects << 'second'
+        e % 20 == 10
+      end.force
+      assert_equal([10, 30], took)
+      assert_equal(%w[
+        second
+        first
+        second
+        first
+        second
+        first
+      ], sub_effects)
+    end
+
     def test_array_remove_without_block
       src = [10, 20, 30, 40]
 
@@ -82,6 +198,12 @@ module IkngttyRubyLibTest
   class EnumerableRefinementsTestWithoutUsing < Test::Unit::TestCase
     def test_enumerable_empty?
       assert_not(Enumerable.method_defined?(:empty?))
+    end
+    def test_enumerable_take_break_if
+      assert_not(Enumerable.method_defined?(:take_break_if))
+    end
+    def test_enumerator_lazy_take_break_if
+      assert_not(Enumerator::Lazy.method_defined?(:take_break_if))
     end
     def test_array_remove
       assert_not(Array.method_defined?(:remove))
